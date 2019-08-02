ccb9118b95fda85125db3d34bf2da9071cf86c8e
idonans <idonans@126.com>
2019-08-02 13:48:39 +0800

    add UnionTypeAdapter support load pre page & load next page

--- app/src/main/java/com/idonans/example/uniontype/UnionTypeAdapter.java
+++ app/src/main/java/com/idonans/example/uniontype/UnionTypeAdapter.java
@@ -2,13 +2,19 @@ package com.idonans.example.uniontype;
 
 import android.view.ViewGroup;
 
+import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.common.base.Preconditions;
 
-public abstract class UnionTypeAdapter extends RecyclerView.Adapter<UnionTypeViewHolder> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class UnionTypeAdapter extends RecyclerView.Adapter<UnionTypeViewHolder> {
+
+    private final List<UnionTypeItemObject> mData = new ArrayList<>();
     private Host mHost;
     private UnionTypeMapper mUnionTypeMapper;
 
@@ -18,6 +24,14 @@ public abstract class UnionTypeAdapter extends RecyclerView.Adapter<UnionTypeVie
 
     public void setUnionTypeMapper(@NonNull UnionTypeMapper unionTypeMapper) {
         mUnionTypeMapper = unionTypeMapper;
+    }
+
+    public Host getHost() {
+        return mHost;
+    }
+
+    public UnionTypeMapper getUnionTypeMapper() {
+        return mUnionTypeMapper;
     }
 
     @NonNull
@@ -48,12 +62,73 @@ public abstract class UnionTypeAdapter extends RecyclerView.Adapter<UnionTypeVie
         if (itemObject != null) {
             holder.onBind(position, itemObject.itemObject);
         }
+
+        if (mOnLoadPrePageListener != null) {
+            if (position <= mLoadPrePageOffset) {
+                mOnLoadPrePageListener.onLoadPrePage();
+            }
+        }
+        if (mOnLoadNextPageListener != null) {
+            int count = getItemCount();
+            if (count - position - 1 <= mLoadNextPageOffset) {
+                mOnLoadNextPageListener.onLoadNextPage();
+            }
+        }
+
     }
 
-    public abstract UnionTypeItemObject getItem(int position);
+    @Nullable
+    public UnionTypeItemObject getItem(int position) {
+        int size = mData.size();
+        if (position >= 0 && position < size) {
+            return mData.get(position);
+        }
+        return null;
+    }
+
+    @NonNull
+    public List<UnionTypeItemObject> getData() {
+        return mData;
+    }
 
     @Override
-    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    /**
+     * 加载上一页
+     */
+    public interface OnLoadPrePageListener {
+        void onLoadPrePage();
+    }
+
+    private int mLoadPrePageOffset = 5;
+    private OnLoadPrePageListener mOnLoadPrePageListener;
+
+    public void setLoadPrePageOffset(@IntRange(from = 0) int offset) {
+        mLoadPrePageOffset = offset;
+    }
+
+    public void setOnLoadPrePageListener(OnLoadPrePageListener listener) {
+        mOnLoadPrePageListener = listener;
+    }
+
+    /**
+     * 加载下一页
+     */
+    public interface OnLoadNextPageListener {
+        void onLoadNextPage();
+    }
+
+    private int mLoadNextPageOffset = 5;
+    private OnLoadNextPageListener mOnLoadNextPageListener;
+
+    public void setLoadNextPageOffset(@IntRange(from = 0) int offset) {
+        mLoadNextPageOffset = offset;
+    }
+
+    public void setOnLoadNextPageListener(OnLoadNextPageListener listener) {
+        mOnLoadNextPageListener = listener;
     }
 }
