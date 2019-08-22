79e65449122426e98d8a4927715075247e2261c4
idonans <idonans@126.com>
2019-08-22 22:44:24 +0800

    update dynamic to 0.1.18

--- app/src/main/java/com/idonans/example/module/uniontype/impl/UnionType.java
+++ app/src/main/java/com/idonans/example/module/uniontype/impl/UnionType.java
@@ -1,8 +1,8 @@
 package com.idonans.example.module.uniontype.impl;
 
-import com.idonans.dynamic.page.uniontype.UnionTypePageStatus;
+import com.idonans.dynamic.uniontype.loadingstatus.UnionTypeLoadingStatus;
 
-public class UnionType extends UnionTypePageStatus {
+public class UnionType extends UnionTypeLoadingStatus {
 
     public static final int UNION_TYPE_TEXT = 1;
     public static final int UNION_TYPE_GITHUB_USER_INFO = 2;
