5b061b23f72168a6ecb3186b31b120da4b163fea
idonans <idonans@126.com>
2019-08-27 16:41:16 +0800

    add pull layout

---
+++ app/src/main/java/com/idonans/example/widget/pulllayout/SimplePullHeader.java
@@ -0,0 +1,44 @@
+package com.idonans.example.widget.pulllayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.AttrRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StyleRes;
+
+public class SimplePullHeader extends FrameLayout implements PullLayout.Header {
+
+    public SimplePullHeader(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public SimplePullHeader(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SimplePullHeader(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initFromAttributes(context, attrs, defStyleAttr, 0);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SimplePullHeader(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initFromAttributes(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    protected void initFromAttributes(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    }
+
+    @Override
+    public void updateOffset(@NonNull PullLayout.OffsetHelper offsetHelper, boolean animating, int windowOffsetX, int windowOffsetY, @NonNull PullLayout pullLayout) {
+        setTranslationX(windowOffsetX);
+        setTranslationY(windowOffsetY);
+    }
+
+}
